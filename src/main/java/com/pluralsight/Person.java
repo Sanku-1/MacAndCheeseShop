463a670a593a4ff08baa3e1f827978dba6ca6933
Stephen_A <ankustephen99@gmail.com>
2025-11-10 09:41:57 -0500

    Initial Commit

---
+++ src/main/java/com/pluralsight/Person.java
@@ -0,0 +1,35 @@
+package com.pluralsight;
+
+public class Person {
+    String name;
+    double rewardPoints;
+    double phoneNumber;
+    String email;
+    String password;
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Person(String name, String email, double phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void addRewardPoints(double newPoints) {
+        this.rewardPoints += newPoints;
+        System.out.println("You now have " + rewardPoints + " reward points!");
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+}
---
+++ src/main/java/com/pluralsight/Topping.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+public class Topping {
+    String name;
+    boolean isPremium;
+    double baseCost;
+    String productType;
+
+    public Topping(String name, boolean isPremium, double baseCost, String productType) {
+        this.name = name;
+        this.isPremium = isPremium;
+        this.baseCost = baseCost;
+        this.productType = productType;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
+    public double getValue() {
+        return null;
+    }
+}
