a84d28775bf981c6e728c4e28cbab5a2f9052731
Stephen_A <ankustephen99@gmail.com>
2025-11-13 21:22:50 -0500

    Created StandardTopping class that extends Topping abstract class

---
+++ src/main/java/com/pluralsight/shop/StandardTopping.java
@@ -0,0 +1,9 @@
+package com.pluralsight.shop;
+
+public class StandardTopping extends Topping{
+
+    public StandardTopping(String name, double baseCost, String productType) {
+        super(name, baseCost, productType);
+    }
+
+}
