18132edec6cf12431f08be4ec6248155964a955e
Stephen_A <ankustephen99@gmail.com>
2025-11-13 22:01:51 -0500

    Completed getCost() and getToppingCost() methods for MacAndCheese class in order to calculate cost of an ordered bowl

--- src/main/java/com/pluralsight/shop/MacAndCheese.java
+++ src/main/java/com/pluralsight/shop/MacAndCheese.java
@@ -1,26 +1,51 @@
 package com.pluralsight.shop;
 import java.util.ArrayList;
+import java.util.List;
 
-public class MacAndCheese extends Product{
+public class MacAndCheese extends Product {
     String flavor; //(Buffalo, BBQ, Plain, Four Cheese)
     ArrayList<Topping> toppingsList;
-    boolean hasExtraCheese; //(work on it)
+    boolean isExtraCheese;//(work on it)
+    boolean isExtraMeat;
 
-    public MacAndCheese(String productType, String size, int productId, String flavor) {
+    public MacAndCheese(String productType, String size, int productId, String flavor, boolean isExtraCheese, boolean isExtraMeat) {
         super(productType, size, productId);
         toppingsList = new ArrayList<>();
         this.flavor = flavor;
+        this.isExtraCheese = isExtraCheese;
+        this.isExtraMeat = isExtraMeat;
     }
 
     @Override
     public double getCost() {
+        double baseCost = 0;
+        double extrasCost = 0;
         if (size.equalsIgnoreCase("small")) {
-            return 7;
+            baseCost = 3.5;
+            if (isExtraCheese) {
+                extrasCost += 0.3;
+            }
+            if (isExtraMeat) {
+                extrasCost += 0.5;
+            }
         } else if (size.equalsIgnoreCase("medium")) {
-            return 8;
+            baseCost = 9;
+            if (isExtraCheese) {
+                extrasCost += 0.6;
+            }
+            if (isExtraMeat) {
+                extrasCost += 1;
+            }
         } else {
-            return 10;
+            baseCost = 8.50;
+            if (isExtraCheese) {
+                extrasCost += 0.9;
+            }
+            if (isExtraMeat) {
+                extrasCost += 1.5;
+            }
         }
+        return baseCost + extrasCost + getToppingCost();
     }
 
     public void addTopping(Topping topping) {
@@ -35,5 +60,60 @@ public class MacAndCheese extends Product{
         }
     }
 
+    public double getToppingCost() {
+        if (size.equalsIgnoreCase("small")) {
+            List<Double> toppingCost = toppingsList.stream().map(Topping -> {
+                        if (Topping instanceof MeatTopping) {
+                            return Topping.getValue();
+                        } else if (Topping instanceof PremiumTopping) {
+                            return Topping.getValue();
+                        } else {
+                            //Standard or Condiment Toppings
+                            return Topping.getValue();
+                        }
+                    }
+            ).toList();
 
+            return toppingCost.stream().reduce(
+                    //Set Initial Value
+                    0.0,
+                    (temp, toppingValue) -> temp += toppingValue);
+        } else if (size.equalsIgnoreCase("medium")) {
+            List<Double> toppingCost = toppingsList.stream().map(Topping -> {
+                        if (Topping instanceof MeatTopping) {
+                            return Topping.getValue() * 2;
+                        } else if (Topping instanceof PremiumTopping) {
+                            return Topping.getValue() * 2;
+                        } else {
+                            //Standard or Condiment Toppings
+                            return Topping.getValue();
+                        }
+                    }
+            ).toList();
+
+            return toppingCost.stream().reduce(
+                    //Set Initial Value
+                    0.0,
+                    (temp, toppingValue) -> temp += toppingValue);
+        }
+        else {
+            //Large Size
+            List<Double> toppingCost = toppingsList.stream().map(Topping -> {
+                        if (Topping instanceof MeatTopping) {
+                            return Topping.getValue() * 3;
+                        } else if (Topping instanceof PremiumTopping) {
+                            return Topping.getValue() * 3;
+                        } else {
+                            //Standard or Condiment Toppings
+                            return Topping.getValue();
+                        }
+                    }
+            ).toList();
+
+            return toppingCost.stream().reduce(
+                    //Set Initial Value
+                    0.0,
+                    (temp, toppingValue) -> temp += toppingValue);
+        }
+    }
 }
