db3a42b75137ea101c01b85d6d7adfeb6f2daf78
Stephen_A <ankustephen99@gmail.com>
2025-11-13 21:44:46 -0500

    Created CondimentTopping class that extends Topping abstract class

---
+++ src/main/java/com/pluralsight/shop/CondimentTopping.java
@@ -0,0 +1,7 @@
+package com.pluralsight.shop;
+
+public class CondimentTopping extends Topping{
+    public CondimentTopping(String name, double baseCost, String productType) {
+        super(name, baseCost, productType);
+    }
+}
