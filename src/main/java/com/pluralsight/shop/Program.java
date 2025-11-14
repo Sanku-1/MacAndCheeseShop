f8dd628276ec0bd78ca01ee8cffa76091b3c9f77
Stephen_A <ankustephen99@gmail.com>
2025-11-13 21:19:18 -0500

    Created Program class to launch shop with main() method

---
+++ src/main/java/com/pluralsight/shop/Program.java
@@ -0,0 +1,10 @@
+package com.pluralsight.shop;
+
+import java.io.IOException;
+
+public class Program {
+        public static void main(String[] args) throws IOException {
+            UserInterface storefrontUserInterface = new UserInterface();
+            storefrontUserInterface.display();
+        }
+}
