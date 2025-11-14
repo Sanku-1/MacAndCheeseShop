9b85b51c704262a5910d435d1943567bedf1d05e
Stephen_A <ankustephen99@gmail.com>
2025-11-14 08:31:28 -0500

    Commenting out code in Order.java due to lack of use, but saving for future feature addition

--- src/main/java/com/pluralsight/shop/Order.java
+++ src/main/java/com/pluralsight/shop/Order.java
@@ -1,42 +1,42 @@
-package com.pluralsight.shop;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-
-public class Order{
-    double taxRate;
-    double tipPercentage;
-    double tipAmount;
-    LocalDateTime timePlaced;
-    Cart newOrder;
-    boolean isTipPercentage;
-    double totalCost;
-
-    public Order(Cart newOrder, LocalDateTime timePlaced, double tipAmount, double taxRate) {
-        this.newOrder = newOrder;
-        this.timePlaced = LocalDateTime.now();
-        this.tipAmount = tipAmount;
-        this.taxRate = taxRate;
-        isTipPercentage = false;
-    }
-
-    public Order(Cart newOrder, double tipPercentage, LocalDateTime timePlaced, double taxRate) {
-        this.newOrder = newOrder;
-        this.tipPercentage = tipPercentage;
-        this.timePlaced = LocalDateTime.now();
-        this.taxRate = taxRate;
-        isTipPercentage = true;
-    }
-
-    public double totalCost() {
-        double cartCostAfterTipTax = newOrder.getBaseCost();
-        if (isTipPercentage) {
-            cartCostAfterTipTax = (newOrder.getBaseCost() * (1 + this.tipPercentage + this.taxRate));
-        } else {
-            cartCostAfterTipTax = (newOrder.getBaseCost() * (1 + this.taxRate) + this.tipAmount);
-        }
-        totalCost = cartCostAfterTipTax;
-        return cartCostAfterTipTax;
-    }
-
-
-}
+//package com.pluralsight.shop;
+//import java.time.LocalDateTime;
+//import java.util.ArrayList;
+//
+//public class Order{
+//    double taxRate;
+//    double tipPercentage;
+//    double tipAmount;
+//    LocalDateTime timePlaced;
+//    Cart newOrder;
+//    boolean isTipPercentage;
+//    double totalCost;
+//
+//    public Order(Cart newOrder, LocalDateTime timePlaced, double tipAmount, double taxRate) {
+//        this.newOrder = newOrder;
+//        this.timePlaced = LocalDateTime.now();
+//        this.tipAmount = tipAmount;
+//        this.taxRate = taxRate;
+//        isTipPercentage = false;
+//    }
+//
+//    public Order(Cart newOrder, double tipPercentage, LocalDateTime timePlaced, double taxRate) {
+//        this.newOrder = newOrder;
+//        this.tipPercentage = tipPercentage;
+//        this.timePlaced = LocalDateTime.now();
+//        this.taxRate = taxRate;
+//        isTipPercentage = true;
+//    }
+//
+//    public double totalCost() {
+//        double cartCostAfterTipTax = newOrder.getBaseCost();
+//        if (isTipPercentage) {
+//            cartCostAfterTipTax = (newOrder.getBaseCost() * (1 + this.tipPercentage + this.taxRate));
+//        } else {
+//            cartCostAfterTipTax = (newOrder.getBaseCost() * (1 + this.taxRate) + this.tipAmount);
+//        }
+//        totalCost = cartCostAfterTipTax;
+//        return cartCostAfterTipTax;
+//    }
+//
+//
+//}
