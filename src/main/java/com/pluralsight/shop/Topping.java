bdb2fc06cc0e4ae387d0ab8ed2183731bba22455
Stephen_A <ankustephen99@gmail.com>
2025-11-13 20:32:54 -0500

    Updated getValue() method in Topping class

--- src/main/java/com/pluralsight/shop/Topping.java
+++ src/main/java/com/pluralsight/shop/Topping.java
@@ -2,22 +2,17 @@ package com.pluralsight.shop;
 
 public class Topping {
     String name;
-    boolean isPremium;
     double baseCost;
     String productType;
 
     public Topping(String name, boolean isPremium, double baseCost, String productType) {
         this.name = name;
-        this.isPremium = isPremium;
         this.baseCost = baseCost;
         this.productType = productType;
     }
 
-    public boolean isPremium() {
-        return isPremium;
-    }
 
     public double getValue() {
-        return null;
+        return 1;
     }
 }
